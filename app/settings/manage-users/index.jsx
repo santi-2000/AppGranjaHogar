5c4ea25e23b821e6817e151f801afe6da6951ff2
Alejandro Ortiz <carlosortizz1205@gmail.com>
2025-09-29 21:33:42 -0600

    emails added

--- app/settings/manage-users/index.jsx
+++ app/settings/manage-users/index.jsx
@@ -17,15 +17,15 @@ export default function ManageUsersScreen() {
       </View>
 
       <View className="mb-2">
-          <LinkUsers width={50} height={50} directory={"/settings/manage-users/edit-user/0"} text={"Yahir Tapia"} />
+          <LinkUsers width={50} height={50} directory={"/settings/manage-users/edit-user/0"} text={"Yahir Tapia"} email={"yahir.tapia@granjahogar.com"}/>
       </View>
 
       <View className="mb-2">
-          <LinkUsers bg={"#0f8d3dff"} width={50} height={50} directory={"/settings/manage-users/edit-user/1"} text={"Ana Maria"} />
+          <LinkUsers bg={"#0f8d3dff"} width={50} height={50} directory={"/settings/manage-users/edit-user/1"} text={"Ana Lopez"} email={"ana.lopez@granjahogar.com"}/>
       </View>
     
       <View className="mb-2">
-          <LinkUsers bg={"#4F3A95"} width={50} height={50} directory={"/settings/manage-users/edit-user/2"} text={"Jared Marquez"} />
+          <LinkUsers bg={"#4F3A95"} width={50} height={50} directory={"/settings/manage-users/edit-user/2"} text={"Jared Marquez"} email={"jared.marquez@granjahogar.com"}/>
       </View>
 
     </View>
--- components/Users/LinkUsers.jsx
+++ components/Users/LinkUsers.jsx
@@ -4,14 +4,17 @@ import { Svg, Path } from 'react-native-svg'
 import MoreHorizontal from '../icons/MoreHorizontal.jsx';
 import Avatar from '../Profile/Avatar.jsx';
 
-export default function LinkUsers({ bg, width, height, directory, text }) {
+export default function LinkUsers({ bg, width, height, directory, text, email }) {
     return (
         <View className="p-4 rounded-xl bg-white border flex border-[#D9D9D9] w-full">
             <Link href={directory}>
                 <View className="w-full flex-row justify-between items-center">
                     <View className="flex-row items-center">
                         {Avatar({ title: text[0], bg: bg, width: width, height: height })}
-                        <Text className="ml-3">{text}</Text>
+                        <View className="ml-4">
+                        <Text className="mb-1 font-semibold">{text}</Text>
+                        <Text className="text-sm text-second">{email}</Text>
+                        </View>
                     </View>
                     <View>
                         <MoreHorizontal/>
