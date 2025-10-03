335adc0f0e620909d2ea36095a772be183bdee5c
Jared Marquez <A01563604@tec.mx>
2025-10-03 10:00:02 -0600

    base endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 import express from "express";
+import rolRoute from "./routes/roles.route.js"
 
 const app = express()
 
 app.use(express.json())
+app.use("/v1/roles", rolRoute)
 
-app.use("/", (req, res) =>{
-    res.json({"jared":"Marquez"})
-})
 
 export default app
---
+++ backend/controllers/roles.controller.js
@@ -0,0 +1,13 @@
+export const getRol = (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Aqui va el rol</h1>
+</body>
+</html>`)
+}
