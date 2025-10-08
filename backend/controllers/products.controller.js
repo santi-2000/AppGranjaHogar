00258314972b193b38374533de1a4481865b00fd
Alejandro Ortiz <carlosortizz1205@gmail.com>
2025-10-08 02:03:44 -0600

    create/delete product added (delete por revisar)

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,4 +1,4 @@
-import { getCatalogService } from "../services/products.service.js";
+import { getCatalogService, createProductService, deleteProductService } from "../services/products.service.js";
 import { validationResult } from 'express-validator'
 
 export const getCatalog = async (req, res) => {
@@ -10,6 +10,48 @@ export const getCatalog = async (req, res) => {
         const catalog = await getCatalogService()
 
         res.json(catalog)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ "Error": "Error" });
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        let result = validationResult(req);
+
+        if (result.errors.length > 0) return res.status(400).json({ success: false, error: result });
+
+        const { category_id, unit_id, name, perishable, min_stock, max_stock } = req.body;
+
+        const creationResult = await createProductService(category_id, unit_id, name, perishable, min_stock, max_stock);
+
+        if (!creationResult.success) {
+            return res.status(500).json(creationResult);
+        }
+
+        res.json(creationResult);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ "Error": "Error" });
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        let result = validationResult(req);
+
+        if (result.errors.length > 0) return res.status(400).json({ success: false, error: result });
+
+        const { id } = req.params;
+
+        const deletionResult = await deleteProductService(id);
+
+        if (!deletionResult.success) {
+            return res.status(500).json(deletionResult);
+        }
+
+        res.json(deletionResult);
     } catch (error) {
         console.log(error)
         res.status(500).json({ "Error": "Error" });
--- backend/models/products.model.js
+++ backend/models/products.model.js
@@ -3,3 +3,13 @@ import db from "./index.js"
 export const getCatalogModel = async () => {
     return await db.query('SELECT * FROM products');
 }
+
+export const createProductModel = async (category_id, unit_id, name, perishable, min_stock, max_stock) => {
+    const sql = 'INSERT INTO products (category_id, unit_id, name, perishable, min_stock, max_stock, actual_stock, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, 0, NOW(), NOW())';
+    return await db.query(sql, [category_id, unit_id, name, perishable, min_stock, max_stock]);
+}
+
+export const deleteProductModel = async (id) => {
+    const sql = 'DELETE FROM products WHERE id = ?';
+    return await db.query(sql, [id]);
+}  
