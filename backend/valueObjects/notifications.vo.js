455e4674015a122e47999c9507366aa7eb73ee24
Sil <a01779075@tec.mx>
2025-10-08 14:27:37 -0600

    aaaaaa

---
+++ backend/valueObjects/notifications.vo.js
@@ -0,0 +1,12 @@
+class NotificationVO {
+    constructor(id, product_id, product_entry_id, content, notification_type, created_at)  {
+        this.id = id;
+        this.product_id = product_id;
+        this.product_entry_id = product_entry_id;
+        this.content = content;
+        this.notification_type = notification_type;
+        this.created_at = created_at;
+    }
+}
+
+module.exports = NotificationVO;
