8a871dd825707f2d84c96b378ddb440fe9174613
lughx <lughx@pm.me>
2025-10-09 12:23:24 -0600

    names and others fixed

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,17 +1,13 @@
-import { getPool, sql } from "../database/mssql.js";
+import db from "../models/index.js"
 
 export const UsersModel = {
-  async create({ name, last_name, passwordHash }) {
-    const pool = await getPool();
-    const res = await pool.request()
-      .input("name", sql.NVarChar(120), String(name ?? "").trim())
-      .input("last_name", sql.NVarChar(120), String(last_name ?? "").trim())
-      .input("password", sql.VarChar(255), passwordHash)
-      .query(`
-        INSERT INTO [Users] (name, last_name, password)
-        OUTPUT inserted.id, inserted.name, inserted.last_name
-        VALUES (@name, @last_name, @password)
-      `);
-    return res.recordset[0]; // { id, name, last_name }
+  async create({ name, last_name, username, passwordHash }) {
+    const sql = `
+      INSERT INTO users (name, username, last_name, password_hash)
+      VALUES (?, ?, ?, ?)
+    `;
+    const params = [name, username, last_name, passwordHash];
+    const [result] = await db.query(sql, params);
+    return result;
   }
 };
--- backend/valueObjects/products/ProductQuantity.vo.js
+++
@@ -1,7 +0,0 @@
-export class ProductQuantityVO {
-    constructor(dbProduct) {
-        this.name = dbProduct.name;
-        this.quantity = dbProduct.actual_stock || 0;
-        this.unit = dbProduct.unit_id || 'unidad';
-    }
-}
