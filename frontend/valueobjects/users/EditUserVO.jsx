b987c8ee11f06fa4b3aae51c40b3ab3f4e9ab430
lughx <lughx@pm.me>
2025-10-24 14:08:02 -0600

    users working

--- frontend/valueobjects/users/EditUserVO.jsx
+++ frontend/valueobjects/users/EditUserVO.jsx
@@ -1,5 +1,6 @@
 export class EditUserVO {
-    constructor({ name, lastName, permissions }) {
+    constructor({ id, name, lastName, permissions }) {
+        this.id = id
         this.name = name;
         this.lastName = lastName;
         this.permissions = permissions;
